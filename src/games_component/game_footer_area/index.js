b2953a976b51f094ce3093c2a64ed552eb6d0a28
jisdom <jisdom2020@gmail.com>
2024-03-28 13:26:09 +0800

    百家樂新增兩個組件: 1:打賞小費組件 2:增加賭桌組件

--- src/games_component/game_footer_area/index.js
+++ src/games_component/game_footer_area/index.js
@@ -2,6 +2,8 @@ import './index.scss';
 import GameChipsButton from 'games_component/game_buttons/game_chips_btn';
 import GameBalance from 'games_component/game_balance';
 import GameTotalBet from 'games_component/game_total_bet';
+import GameTips from 'games_component/game_tips';
+import GameAddTable from 'games_component/game_add_table';
 
 const GameFooterArea = () => {
 
@@ -21,7 +23,14 @@ const GameFooterArea = () => {
                         <GameChipsButton />
                     </div>
                 </div>
-                <div className='right-box'></div>
+                <div className='right-box'>
+                    <div className='box-area'>
+                        <GameTips />
+                    </div>
+                    <div className='box-area'>
+                        <GameAddTable />
+                    </div>
+                </div>
             </div>
         </div>
     )
