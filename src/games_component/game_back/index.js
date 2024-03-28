16def12401d053d273175ea1277ff1262645ff74
jisdom <jisdom2020@gmail.com>
2024-03-28 14:55:04 +0800

    game_back組件連動 redux與相關事件處理

--- src/games_component/game_back/index.js
+++ src/games_component/game_back/index.js
@@ -1,12 +1,73 @@
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+    setDefaultClick,
+    setTotalChips1,
+    setTotalChips2,
+    setTotalChips3,
+    setTotalChips4,
+    setTotalChips5,
+    setIsAct1,
+    setIsAct2,
+    setIsAct3,
+    setIsAct4,
+    setIsAct5,
+} from 'store/actions';
 import './index.scss';
-const GameBack = () => {
+const GameBack = (props) => {
+
+    const handleCancel = () => {
+        props.setTotalChips1(0);
+        props.setTotalChips2(0);
+        props.setTotalChips3(0);
+        props.setTotalChips4(0);
+        props.setTotalChips5(0);
+        props.setIsAct1('');
+        props.setIsAct2('');
+        props.setIsAct3('');
+        props.setIsAct4('');
+        props.setIsAct5('');
+        props.setDefaultClick('show')
+    }
 
     return (
-        <div className='game-back-box'>
+        <div className='game-back-box' onClick={handleCancel}>
             <Link to="/" />
         </div>
     )
 }
 
-export default GameBack;
+const mapStateToProps = (state) => {
+    return {
+        defaultClick: state.root.defaultClick,
+        totalChips1: state.root.totalChips1,
+        totalChips2: state.root.totalChips2,
+        totalChips3: state.root.totalChips3,
+        totalChips4: state.root.totalChips4,
+        totalChips5: state.root.totalChips5,
+        isAct1: state.root.isAct1,
+        isAct2: state.root.isAct2,
+        isAct3: state.root.isAct3,
+        isAct4: state.root.isAct4,
+        isAct5: state.root.isAct5,
+    };
+};
+
+
+const mapDispatchToProps = (dispatch) => ({
+
+    setTotalChips1: (totalChips1) => dispatch(setTotalChips1(totalChips1)),
+    setTotalChips2: (totalChips2) => dispatch(setTotalChips2(totalChips2)),
+    setTotalChips3: (totalChips3) => dispatch(setTotalChips3(totalChips3)),
+    setTotalChips4: (totalChips4) => dispatch(setTotalChips4(totalChips4)),
+    setTotalChips5: (totalChips5) => dispatch(setTotalChips5(totalChips5)),
+    setIsAct1: (isAct1) => dispatch(setIsAct1(isAct1)),
+    setIsAct2: (isAct2) => dispatch(setIsAct2(isAct2)),
+    setIsAct3: (isAct3) => dispatch(setIsAct3(isAct3)),
+    setIsAct4: (isAct4) => dispatch(setIsAct4(isAct4)),
+    setIsAct5: (isAct5) => dispatch(setIsAct5(isAct5)),
+    setDefaultClick: (defaultClick) => dispatch(setDefaultClick(defaultClick)),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameBack);
