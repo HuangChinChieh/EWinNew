ee125fecf90d5109601dbc7438c152bed8045928
jisdom <jisdom2020@gmail.com>
2024-04-11 11:58:41 +0800

    百家樂內選擇視頻線路組件撰寫與api對接

--- src/games_component/game_buttons/game_hdsd_btn/index.js
+++ src/games_component/game_buttons/game_hdsd_btn/index.js
@@ -1,11 +1,17 @@
 import { useState, useRef, useEffect } from 'react';
 import { useLanguage } from 'hooks';
+import { useLobbyContext } from 'provider/GameLobbyProvider';
 import './index.scss';
 
 const GameHDSDButton = () => {
-    const { t } = useLanguage();
+    const {
+        t,
+        tiList
+    } = useLobbyContext();
     const [hoveredItem, setHoveredItem] = useState(null);
     const settingsRef = useRef(null);
+    const localTitle = localStorage.getItem('gameTitle');
+    const [selectedLimit, setSelectedLimit] = useState(null);
 
     const handleDocumentClick = (e) => {
         if (settingsRef.current && !settingsRef.current.contains(e.target)) {
@@ -13,6 +19,21 @@ const GameHDSDButton = () => {
             setHoveredItem(null);
         }
     };
+
+    const handleOptionClick = (id) => {
+        setSelectedLimit(id);
+        setHoveredItem(null);
+    };
+
+    useEffect(() => {
+        if (tiList && tiList.TableInfoList) {
+            const tableInfo = tiList.TableInfoList.find(table => table.TableNumber === localTitle);
+            if (tableInfo && tableInfo.Stream && tableInfo.Stream.length > 0) {
+                setSelectedLimit(tableInfo.Stream[0].StreamName);
+            }
+        }
+    }, [tiList, localTitle]);
+
 
     useEffect(() => {
         // 在 component mount 時加入 click 事件監聽器
@@ -35,7 +56,23 @@ const GameHDSDButton = () => {
                 <div className={`hover-box ${hoveredItem === 1 ? 'visible' : ''}`}>
                     <div className='title'>{t("VideoLine.title")}</div>
                     <div className='dis'>
-
+                        {tiList && tiList.TableInfoList && tiList.TableInfoList.map((tableInfo, tableIndex) => (
+                            (tableInfo.TableNumber === localTitle) && (
+                                <div
+                                    key={tableIndex}
+                                >
+                                    {tableInfo.Stream && tableInfo.Stream.map((stream, streamIndex) => (
+                                        <span
+                                            key={streamIndex}
+                                            onClick={() => handleOptionClick(stream.StreamName)}
+                                            className={`option ${selectedLimit === stream.StreamName ? 'selected' : ''}`}
+                                        >
+                                            {stream.StreamName} {stream.StreamResolution === 0 ? 'HD' : (stream.StreamResolution === 1 ? 'SD' : 'PinCamera')}
+                                        </span>
+                                    ))}
+                                </div>
+                            )
+                        ))}
                     </div>
                 </div>
             </div>
