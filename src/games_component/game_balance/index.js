01f4d513ebabd2c51a47277c84f4350d4a10cc3d
jisdom <jisdom2020@gmail.com>
2024-04-15 17:49:13 +0800

    新增 hardcode 項目

--- src/games_component/game_balance/index.js
+++ src/games_component/game_balance/index.js
@@ -11,7 +11,9 @@ const GameBalance = (props) => {
             <span className='icon-box'>{t("Global.balance")}: </span>
             <span>
                 {props.userInfo && props.userInfo.Wallet && props.userInfo.Wallet.map((i, index) => (
-                    i.CurrencyType === props.userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{i.Balance}</span> : ''
+                    // 登入後BetLimitCurrencyType全部變成空字串 暫時用 hardcode製作
+                    // i.CurrencyType === props.userInfo.BetLimitCurrencyType ? <span className='without-mr' key={index}>{i.Balance}</span> : ''
+                    i.CurrencyType === localStorage.getItem('CurrencyType') ? <span className='without-mr' key={index}>{i.Balance}</span> : ''
                 ))}
             </span>
         </div>
