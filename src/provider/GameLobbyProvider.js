3602c2ecffc581ff75db478bed603ce06caa7ccb
meteor950187 <meteor950187@hotmail.com>
2024-04-23 19:58:10 +0800

    1

--- src/provider/GameLobbyProvider.js
+++ src/provider/GameLobbyProvider.js
@@ -7,85 +7,26 @@ const WalletContext = createContext();
 const RealNameContext = createContext();
 const LanguageContext = createContext();
 const BetLimitCurrencyContext = createContext();
-import {  generateUUIDv4} from "../utils/guid";
+
 // 建立一個 Context
 const GameLobbyContext = createContext();
 
 // 提供一個 Custom Hook 讓其他元件可以存取 Context 值
 export const useLobbyContext = () => useContext(GameLobbyContext);
 
-// 最上層的元件，設置各初始值
-const GameLobbyProvider = ({ children }) => {
-    const GUID=generateUUIDv4();
-    const { t } = useLanguage();
-    const EWinUrl = 'https://ewin.dev.mts.idv.tw';
-    const [CT, setCT] = useState('');
-    const [wallet,setWallect]=useState([]);
-    const [realName,setRealName]=useState('');
-    const [showMessage,setShowMessage]=useState('');
-    
-    const [betLimitCurrencyType,setBetLimitCurrencyType]=useState('');
-    const Echo = 'Test_Echo';
-    // const [domain, setDomain] = useState('');
-    // const [Favos, setFavos] = useState([]);
-    // const [shoeResults, setShoeResults] = useState('');
-    // const [isFavorited, setIsFavorited] = useState(false);
-    // const [isTradition,setIsTradition]=useState(false);
-
-    // Lobby 相關資料
-
-    const [isLoading, setIsLoading] = useState(false);
-    const [newInstance, setNewInstance] = useState([]);
-
-    // 列表資料
-    // const [tiList, setTiList] = useState([]);
-    // 使用者資料
-    const [userInfo, setUserInfo] = useState([]);
-
-
-    useEffect(() => {
-        // 開發時設定每5分鐘打一次api來獲取有效的 CT
-        const fetchDataBySeconds = async () => {
-          try {
-            const response = await fetch(
-              'https://ewin.dev.mts.idv.tw/API/LoginAPI.asmx/UserLoginByCustomValidate?Token=1_0UE5XQQ_ca95cc8bfb4e442118d60c5b92a7af2e&LoginAccount=ddt1&LoginPassword=1234&CompanyCode=demo&UserIP='
-            );
-            const xmlText = await response.text();
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
-            const newCT = xmlDoc.getElementsByTagName('CT')[0].textContent;
-    
-            setCT(newCT);
-            localStorage.setItem('CT', newCT);
-    
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-        const intervalId = setInterval(fetchDataBySeconds, 120000);
-        fetchDataBySeconds();
-        return () => clearInterval(intervalId);
-    
-      }, []);
-
-    // 遊戲大廳
-    const gameLobbyClient = EWinGameLobbyClient.getInstance(CT, EWinUrl);
-
-
 // Create a Context Provider to provide shared values
-const GameLobbyProvider  = ({ children }) => {
-  const { t } = useLanguage();
-  const gameLobbyClient = EWinGameLobbyClient.getInstance();
-  const [wallet, setWallet] = useState([]);
+const GameLobbyProvider = ({ children }) => {
+  const lobbyClient = EWinGameLobbyClient.getInstance();
+  const [wallet, setWallet] = useState(null);
   const [realName, setRealName] = useState('');
   const [betLimitCurrencyType, setBetLimitCurrencyType] = useState('');
-  
+
   // Game Lobby related useEffect
   useEffect(() => {
-    if (gameLobbyClient !== null) {
+    if (lobbyClient !== null) {
       const handleConnected = () => {
         // Get user info
-        gameLobbyClient.GetUserInfo((s, o) => {
+        lobbyClient.GetUserInfo((s, o) => {
           if (s) {
             if (o.ResultCode === 0) {
               console.log('UserInfo', o);
@@ -102,10 +43,34 @@ const GameLobbyProvider  = ({ children }) => {
         });
       };
     }
-  }, [gameLobbyClient]);
+  }, []);
+
+  const updateInfo = (cb) => {
+    lobbyClient.GetUserInfo((s, o) => {
+      if (s) {
+        if (o.ResultCode === 0) {
+          cb(o);
+        }
+      }
+    });
+  };
+
+  const updateWallet = () =>{
+    updateInfo((userInfo) =>{
+      let wallet = userInfo.Wallet.find(x => x.CurrencyType === "CNY");
+      let setObj = {
+        CurrencyType: wallet.CurrencyType,
+        CurrencyName: wallet.CurrencyName,
+        Balance: wallet.Balance        
+      };
+      setWallet(setObj);
+    });
+  };
+
+ 
 
   return (
-    <WalletContext.Provider value={{ wallet, setWallet }}>
+    <WalletContext.Provider value={{ wallet, updateWallet }}>
       <RealNameContext.Provider value={{ realName, setRealName }}>
         <LanguageContext.Provider value={t}>
           <BetLimitCurrencyContext.Provider value={betLimitCurrencyType}>
@@ -117,4 +82,4 @@ const GameLobbyProvider  = ({ children }) => {
   );
 };
 
-export default GameLobbyProvider ;
+export default GameLobbyProvider;
