1381f719033be4a6ac5b223ece9c491b87ddda3f
meteor950187 <meteor950187@hotmail.com>
2024-12-19 17:57:29 +0800

    1

--- src/games_component/game_miniTable_multiBet/index.js
+++ src/games_component/game_miniTable_multiBet/index.js
@@ -25,7 +25,7 @@ const MiniTableMultiBet = ({ tableName,
 
     const [orderData, dispatchOrderData] = useReducer(
         orderReducer,
-        initialOrderData
+        initialOrderData()
     );
 
     const orderDataInfoRef = useRef(orderData);
